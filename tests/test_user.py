f106a80682169148fe6475a6a646b5f118fef956
Qde <nzemka0@gmail.com>
2023-12-26 20:53:57 +0200

    update tests with allure fixtures

--- tests/test_user.py
+++ tests/test_user.py
@@ -4,22 +4,22 @@ import pytest
 from utils.request import Request as request
 
 
-@allure.suite("User Api Call")
+@allure.suite("User API tests")
 class TestUser:
     username = 'Qde13'
 
-    @allure.title("Check is user get 200")
+    @allure.title("Check that user get call is 200")
     @pytest.mark.api
     def test_get_user(self):
         response = request.get_user_response(self, username=self.username)
-        assert response.status_code == 200
+        assert response.status_code == 200, 'User is not found, or status code is different'
 
-    @allure.title("Check is user data present")
+    @allure.title("Check that user data is present")
     @pytest.mark.api
     def test_user_data(self):
         response = request.get_user_response(self, username=self.username)
         user_data = response.json()
-        assert user_data['login'] == self.username
-        assert 'id' in user_data
-        assert 'name' in user_data
-        assert 'email' in user_data or 'public_email' in user_data
+        assert user_data['login'] == self.username, 'User is not found, or data is different'
+        assert 'id' in user_data, 'User is not found, or data is different'
+        assert 'name' in user_data, 'User is not found, or data is different'
+        assert 'email' in user_data or 'public_email' in user_data, 'User is not found, or data is different'
